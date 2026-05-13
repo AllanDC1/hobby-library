d427b695c218a9d218d85c2c6f701b876a119d48
AllanDC1 <adonetticalen@gmail.com>
2026-05-13 17:07:15 -0300

    erro no backend para email já cadastrado

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -9,6 +9,11 @@ router = APIRouter(prefix="/api/users", tags=["Users"])
 def register_user(payload: UserCreate):
     existing = get_user_by_email(payload.email)
     if existing:
+        if existing["username"] != payload.username:
+            raise HTTPException(
+                status_code=401,
+                detail="Conta já registrada com esse email."
+            )
         return existing
     user = create_user(payload.username, payload.email)
     return user
